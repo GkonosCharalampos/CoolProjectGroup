564968a8b53ed043719b37842d8ad323d3893ef8
Jan Wilken Doerrie <jwdoerrie@gmail.com>
2014-03-17 17:00:06 +0100

    Added extract of all roads in Zurich usable by cars. Also fixed ways by removing invalid node references.

---
+++ code/OSM_Fix_Ways.py
@@ -0,0 +1,43 @@
+import re,sys # needed for re.split() and sys.argv()
+
+"""
+This script expects an OpenStreetMap.
+It removes all invalid node references, 
+i.e. it just keeps those where the id was defined.
+"""
+
+
+""" 
+Check if file name is given as command line argument,
+else query user for it
+"""
+file = raw_input() if len(sys.argv) == 1 else sys.argv[1]
+
+"""
+initialize set of nodes
+"""
+nodes = {}
+
+""" 
+iterate through all lines in the given file
+"""
+with open(file) as f:
+	for line in f:
+		if "node id" in line:
+			"""
+			if "node id" is present in the given line split on 
+			quotation mark and store extracted id in the set
+			"""
+			node = int(re.split("['\"]", line)[1])
+			nodes[node] = 1
+
+		elif "nd ref" in line:
+			"""
+			if "nd ref" is present in the given line check
+			if the current id was encountered before, if not skip 
+			"""
+			node = int(re.split("['\"]", line)[1])
+			if node not in nodes:
+				continue
+
+		print line,
