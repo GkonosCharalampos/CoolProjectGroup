711b0bae49f76d87e9b114aa064638ee1104059d
jdoerrie <jwdoerrie@gmail.com>
2014-05-05 19:53:07 +0200

    Modified JavaScript Rendering

--- code/initialize_simulation.m
+++ code/initialize_simulation.m
@@ -3,13 +3,13 @@ function [nodes,sources,sinks,paths,delay] = initialize_simulation()
     numcars = 1000;
     delay = 1000;
 
-    [nodes,edges] = read_graph('/Users/simone/Desktop/primary_roads.txt');
-    
+    [nodes, edges] = read_graph('../data/Zurich_Residential_Roads+_Simplified_Ways_Single_Component_Graph.txt');
+
     numnodes = size(nodes,2);
-    
+
     sources = floor(rand(numcars,1)*numnodes + 1);
     sinks = floor(rand(numcars,1)*numnodes + 1);
-    
+
     paths = dijkstra_mx(nodes,edges,sources',sinks');
 end
 
